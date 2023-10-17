cfc1304bd988f1459132445f21f98229db753507
Denko <officedenko@gmail.com>
2023-10-18 00:44:47 +0300

    server_run_but_doesnt_work

--- hw_web10/app_hw_web10/urls.py
+++ hw_web10/app_hw_web10/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .apps import AppHwWeb10Config
 
-urlpatterns = [path("", views.main, name='root']
+app_name = AppHwWeb10Config.name
+app_name = "App_HwWeb10"
+
+urlpatterns = [path("", views.main, name="root")]
--- hw_web10/app_hw_web10/views.py
+++ hw_web10/app_hw_web10/views.py
@@ -1,6 +1,7 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 
 # Create your views here.
 def main(request):
-    pass
+    return render(request, "app_hw_web10/index.html", context={"title": "Web 10 hw!"})
