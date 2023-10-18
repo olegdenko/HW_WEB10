d4c4eb19565798184d2c04e19e78c32ee7cb0a6b
Denko <officedenko@gmail.com>
2023-10-18 21:38:09 +0300

    doing

--- hw_web10/app_hw_web10/urls.py
+++ hw_web10/app_hw_web10/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 from .apps import AppHwWeb10Config
 
-app_name = AppHwWeb10Config.name
+# app_name = AppHwWeb10Config.name
 app_name = "App_HwWeb10"
 
 urlpatterns = [path("", views.main, name="root")]
